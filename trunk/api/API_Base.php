5d42e9822f05a4f015702da08eee07a4f97318a4
chrisscott <chrisscott@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-15 20:58:31 +0000

    Tagging version 0.5
    git-svn-id: https://plugins.svn.wordpress.org/thermal-api/tags/0.5@713600 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/api/API_Base.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Voce\Thermal;
+
+abstract class API_Base {
+
+	/**
+	 *
+	 * @var \Slim\Slim $app Slim app instance
+	 */
+	public $app;
+
+	/**
+	 *
+	 * @var string $version API version
+	 */
+	protected $version = '';
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param \Slim\Slim $app Slim app reference
+	 */
+	public function __construct( \Slim\Slim $app) {
+
+		$this->app = $app;
+
+		$this->app->notFound( function () use ( $app ) {
+			$data = array(
+				'error' => array(
+					'message' => 'Invalid route'
+				),
+			);
+			$app->contentType( 'application/json' );
+			$app->halt( 400, json_encode( $data ) );
+		} );
+
+	}
+
+	/**
+	 * Registers route
+	 * 
+	 * @param string $method HTTP method
+	 * @param string $pattern The path pattern to match
+	 * @param callback $callback Callback function for route
+	 */
+	public function registerRoute( $method, $pattern, $callback ) {
+		$method = strtolower( $method );
+
+		$valid_methods = array(
+			'get',
+			'post',
+			'delete',
+			'put',
+			'head',
+			'options',
+		);
+
+		if ( ! in_array( $method, $valid_methods ) ) {
+			return false;
+		}
+
+		$match = trailingslashit( API_BASE ) . trailingslashit( 'v' . $this->version ) . $pattern;
+
+		$app = $this->app;
+
+		return $this->app->$method( $match, function() use ( $app, $callback ) {
+
+			$data = call_user_func_array( $callback, func_get_args() );
+			$json = json_encode( $data );
+			$res  = $app->response();
+
+			$res->header('Access-Control-Allow-Origin', '*');
+			if ( isset( $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'] ) ){
+				$res->header('Access-Control-Allow-Headers', $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+			}
+
+			if ( $json_p = $app->request()->get( 'callback' ) ) {
+
+				$app->contentType( 'application/javascript; charset=utf-8;' );
+				$res->write( sprintf( '%s(%s)', $json_p, $json ) );
+
+			} else {
+
+				$app->contentType( 'application/json; charset=utf-8;' );
+				$res->write(json_encode($data), true);
+
+			}
+
+		});
+	}
+
+}
---
+++ trunk/thermal-api.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+  Plugin Name: Thermal API
+  Version:     0.5
+  Plugin URI:  http://thermal-api.com/
+  Description: The power of WP_Query in a RESTful API.
+  Author:      Voce Platforms
+  Author URI:  http://voceplatforms.com/
+ */
+
+namespace Voce\Thermal;
+
+if ( !defined( 'API_BASE' ) ) {
+	define( 'API_BASE', '/wp_api' );
+}
+
+function api_base_url() {
+	return home_url( user_trailingslashit( API_BASE ) );
+}
+
+class API_Dispatcher {
+
+	public function __construct() {
+		//if requested url starts with api_base_url()
+		if ( false !== strpos( $_SERVER['REQUEST_URI'], API_BASE ) ) {
+			add_action( 'wp_loaded', array( $this, 'dispatch_api' ) );
+		}
+	}
+
+	public function dispatch_api() {
+		//determine API version
+		require_once( 'lib/Slim/Slim/Slim.php' );
+		require_once( 'api/v1/API.php' );
+
+		\Slim\Slim::registerAutoloader();
+
+		$app = new \Slim\Slim();
+
+		$v1 = new APIv1( $app );
+
+		$app->run();
+
+		exit;
+	}
+
+}
+
+new API_Dispatcher();
