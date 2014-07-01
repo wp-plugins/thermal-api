e17d53b5f18f157d20fc59449a31d41aecfa52ea
csloisel <csloisel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-01 18:31:27 +0000

    Adding Capistrano deploy support
    git-svn-id: https://plugins.svn.wordpress.org/thermal-api/trunk@941742 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit90cf6ca40c15a331e2fb98b8d524fd2c::getLoader();
+return ComposerAutoloaderInit1e67751dea9412c4641f6d85d072a911::getLoader();
